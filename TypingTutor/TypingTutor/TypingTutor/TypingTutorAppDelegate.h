0961e2c034fab42ced57de087d44da24cea34d34
liudongbao <liudongbao@139.com>
2012-06-09 21:24:52 +0800

    Revert "Revert "完成练习""
    This reverts commit a74a563cb2297b48a8686c15e9f600391dd78b1b.

---
+++ TypingTutor/TypingTutor/TypingTutor/TypingTutorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TypingTutorAppDelegate.h
+//  TypingTutor
+//
+//  Created by  on 12-6-9.
+//  Copyright (c) 2012年 derrik.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TypingTutorAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TypingTutor/TypingTutor/TypingTutor/TypingTutorAppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  TypingTutorAppDelegate.m
+//  TypingTutor
+//
+//  Created by  on 12-6-9.
+//  Copyright (c) 2012年 derrik.org. All rights reserved.
+//
+
+#import "TypingTutorAppDelegate.h"
+
+@implementation TypingTutorAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{ 
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
