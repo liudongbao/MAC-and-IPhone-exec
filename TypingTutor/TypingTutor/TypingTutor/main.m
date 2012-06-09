76746ce455a9b7cdbc17fb3f917ee664d9908943
liudongbao <liudongbao@139.com>
2012-06-09 21:21:01 +0800

    完成练习

---
+++ TypingTutor/TypingTutor/TypingTutor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TypingTutor
+//
+//  Created by  on 12-6-9.
+//  Copyright (c) 2012年 derrik.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
