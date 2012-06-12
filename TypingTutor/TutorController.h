bf5e0c293e7d61fc98b3e610e1213cdf57048da4
liudongbao <liudongbao@139.com>
2012-06-12 21:50:38 +0800

    add NSTimer

---
+++ TypingTutor/TutorController.h
@@ -0,0 +1,28 @@
+//
+//  TutorController.h
+//  TypingTutor
+//
+//  Created by 冬宝 刘冬宝 on 12-6-12.
+//  Copyright (c) 2012年 derrik.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+@class BigLetterView;
+@interface TutorController : NSObject {
+    // Outlets
+    IBOutlet BigLetterView *inLetterView;
+    IBOutlet BigLetterView *outLetterView;
+    // Data
+    NSArray *letters;
+    int lastIndex;
+    // Time
+    NSTimeInterval startTime;
+    NSTimeInterval elapsedTime;
+    NSTimeInterval timeLimit;
+    NSTimer *timer;
+}-
+(IBAction)stopGo:(id)sender;
+- (void)updateElapsedTime;
+- (void)resetElapsedTime;
+- (void)showAnotherLetter;
+@end
